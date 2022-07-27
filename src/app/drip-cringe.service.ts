73adccac71e68a2f4f149507ffef7222346ff3f0
Lukasz Mowinski <lmowinski@deloittece.com>
2022-07-27 19:36:50 +0200

    feat: RxJS to handle changing affiliation of characters

--- src/app/drip-cringe.service.ts
+++ src/app/drip-cringe.service.ts
@@ -1,5 +1,6 @@
 import {LogService} from "./log.service";
 import {Injectable} from "@angular/core";
+import {Subject} from "rxjs";
 
 @Injectable()
 export class DripCringeService {
@@ -8,6 +9,7 @@ export class DripCringeService {
     {name: 'Hubert Kajzerka', affiliation: ''}
   ];
   private logService: LogService;
+  public charactersChanged = new Subject<void>();
 
   constructor(logService: LogService) {
     this.logService = logService;
@@ -27,6 +29,7 @@ export class DripCringeService {
       return ch.name === characterInfo.name;
     })
     this.characters[pos].affiliation = characterInfo.affiliation;
+    this.charactersChanged.next();
     this.logService.writeLog('Changed side of ' + characterInfo.name + ', new affiliation: ' + characterInfo.affiliation);
   }
 
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,16 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {DripCringeService} from "../drip-cringe.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
   characters = [];
   activatedRoute: ActivatedRoute;
   dcService: DripCringeService;
+  loadedAffiliation = 'all';
+  subscription = new Subscription;
 
   constructor(activatedRoute: ActivatedRoute, dcService: DripCringeService) {
     this.activatedRoute = activatedRoute;
@@ -22,7 +25,18 @@ export class ListComponent implements OnInit {
       (params) => {
         // @ts-ignore
         this.characters = this.dcService.getCharacters(params['affiliation'])
+        this.loadedAffiliation = params['affiliation'];
       }
     );
+    this.subscription = this.dcService.charactersChanged.subscribe(
+      () => {
+        // @ts-ignore
+        this.characters = this.dcService.getCharacters(this.loadedAffiliation);
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
